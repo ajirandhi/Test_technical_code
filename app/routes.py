de3017040e57a1e1b8b059af7638c41a74cd06d2
Randhi Bayu Aji <62879720+ajirandhi@users.noreply.github.com>
2024-08-12 15:08:04 +0700

    init commit

--- app/routes.py
+++ app/routes.py
@@ -15,8 +15,6 @@ def index():
             case 'prima':
                 for num in range(1, form.number.data+1):
                     flag = False
-                    if num == 1:
-                        flash(num)
                     elif num > 1:
                         for i in range(2, num):
                             if (num % i) == 0:
